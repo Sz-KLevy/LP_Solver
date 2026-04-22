d1c7d6421886c4e216decfdab6f70e9f792cfe4c
Szász-Krajcsó Milán Levente <241631521+Sz-KLevy@users.noreply.github.com>
2026-04-22 13:53:19 +0200

    UI point refactor
    - points are stored as they would appear in the cordinate system
    - points are now arrays, containing 2 elements(x,y)
    - points are stored in a vector
    - added getPoints()

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <array>
 #include <string>
 #include <memory>
 #include <format>
@@ -108,14 +109,18 @@ class UI : public QWidget{
 public:
     int windowSize[2] = {500, 500};
     int cellSize = 50;
-    std::vector<QPoint> points;
-    QPoint optimalPoint;
+    std::vector<std::array<float,2>> points;
+    std::array<float,2> optimalPoint;
 
 
 
     UI(QWidget *parent = nullptr) : QWidget(parent){
         resize(windowSize[0], windowSize[1]);
-        optimalPoint = QPoint(windowSize[0]/2, windowSize[1]/2);
+        optimalPoint={0.0,0.0};
+    }
+
+    std::vector<std::array<float,2>> getPoints(){
+        return points;
     }
 
 
@@ -131,39 +136,76 @@ private:
 
     void mousePressEvent(QMouseEvent* event) override {
         QPoint click = event->pos();
+        int origo[2] = {((width()/cellSize)/2)*cellSize, ((height()/cellSize)/2)*cellSize};
 
-        points.push_back(QPoint(click.x(), click.y()));
+        points.push_back({(float)(click.x()-origo[0])/cellSize, (float)-(click.y()-origo[1])/cellSize});
+        std::cout << (float)(click.x()-origo[0])/cellSize << "\t" << (float)-(click.y()-origo[1])/cellSize << std::endl;
 
-        int intOptimalPoint[2] = {100, 100}; //this is where the UI asks for the position of the optimal point
-        optimalPoint = QPoint(intOptimalPoint[0], intOptimalPoint[1]);
+        optimalPoint = {1.0,1.0};//this is where the UI asks for the position of the optimal point
 
         update(); //repaints
     }
 
 
     void drawGrid(QPainter& painter, int cellSize){
-        painter.setPen(Qt::black);
+        QPen gridPen(Qt::black);
+        gridPen.setWidth(1);
+        painter.setPen(gridPen);
 
-        for (int x = 0; x < width(); x += cellSize)
-            painter.drawLine(x, 0, x, height());
+        QPen wideGridPen(Qt::black);
+        wideGridPen.setWidth(4);
 
-        for (int y = 0; y < height(); y += cellSize)
-            painter.drawLine(0, y, width(), y);
+        for (int x = 0; x < width(); x += cellSize){
+            if(((width()/cellSize)/2)*cellSize==x){
+                painter.setPen(wideGridPen);
+                painter.drawLine(x,0,x,height());
+                painter.setPen(gridPen);
+
+                int number=(height()/cellSize)/2;
+                for (int y = 0; y < height(); y += cellSize){
+                    painter.drawText(x+5,y+15, QString::number(number));
+                    number--;
+                }
+            }
+            else{painter.drawLine(x, 0, x, height());}
+        }
+
+        for (int y = 0; y < height(); y += cellSize){
+            if(((height()/cellSize)/2)*cellSize==y){
+                painter.setPen(wideGridPen);
+                painter.drawLine(0,y,width(),y);
+                painter.setPen(gridPen);
+
+                int number = -(width()/cellSize)/2;
+                for(int x = 0; x < width(); x+=cellSize){
+                    painter.drawText(x+5, y+15, QString::number(number));
+                    number++;
+                }
+            }
+            else{painter.drawLine(0, y, width(), y);}
+        }
     }
 
     void drawPoints(QPainter& painter){
         painter.setPen(Qt::red);
         painter.setBrush(Qt::red);
 
-        for (const QPoint& p : points) {
-            painter.drawEllipse(p, 5, 5);
+        std::array<int,2> origo = {((width()/cellSize)/2), ((height()/cellSize)/2)};
+
+        for (const std::array<float,2>& p : points) {
+            QPoint point = QPoint((p[0]+origo[0])*cellSize,(-p[1]+origo[1])*cellSize);
+            painter.drawEllipse(point, 5, 5);
         }
     }
 
-    void drawOptimalPoint(QPainter& painter, QPoint optimal){
+    void drawOptimalPoint(QPainter& painter, std::array<float,2> optimal){
         painter.setPen(Qt::darkGreen);
         painter.setBrush(Qt::darkGreen);
-        painter.drawEllipse(optimal, 5, 5);
+
+        std::array<int,2> origo = {((width()/cellSize)/2), ((height()/cellSize)/2)};
+        QPoint point = QPoint((optimalPoint[0]+origo[0])*cellSize, (-optimalPoint[1]+origo[1])*cellSize);
+
+        painter.drawEllipse(point, 5, 5);
     }
 };
 
